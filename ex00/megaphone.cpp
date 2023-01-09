dd10eba945f764d4ea1f75320f7c5bc925410282
Vanderson Antonietti <v.antonietti@icloud.com>
2023-01-08 21:40:51 -0300

    Hello World

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/08 21:21:05 by vantonie          #+#    #+#             */
+/*   Updated: 2023/01/08 21:36:10 by vantonie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+char	*megaphone(char *string) {
+	toupper()
+}
+
+int	main(int argc, char *argv) {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else 
+		megaphone(argv);
+	
+}
+
