ddc9704e844cb1e6feddd835538e167c1c7701a7
Vanderson Antonietti <v.antonietti@icloud.com>
2023-02-22 20:08:43 -0300

    ex00 uppercase for more arguments, need to fix spaces between arguments

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 17:40:27 by vantonie          #+#    #+#             */
-/*   Updated: 2023/02/22 19:05:40 by vantonie         ###   ########.fr       */
+/*   Updated: 2023/02/22 20:06:22 by vantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,25 @@ using std::string;
 
 string	uppercase(string str) {
 	string res;
-	
-	for(int i = 0; i < str.length(); i++)
+	for (int i = 0; i < str.length(); i++)
 		res += toupper(str[i]);
 	return (res);
 }
 
 int	main(int argc, char **argv) {
 	string strCapitalized;
-
-	strCapitalized = uppercase(argv[1]);
+	string tmp;
+	if (argc == 1)
+		cout << "* LOUD UNBEARABLE FEEDBACK NOISE *" << endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			tmp += argv[i];
+			tmp += " ";
+		}
+	}
+	strCapitalized = uppercase(tmp);
 	cout << strCapitalized << endl;
 	return (0);
 }
