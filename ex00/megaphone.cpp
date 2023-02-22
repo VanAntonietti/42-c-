1e60a58899df8d5d3b1d0b3190231d186c5baf0a
Vanderson Antonietti <v.antonietti@icloud.com>
2023-02-22 19:07:04 -0300

    ex00 achieved to uppercase string, need to check if there are any condition to manage

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,23 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/08 21:21:05 by vantonie          #+#    #+#             */
-/*   Updated: 2023/01/08 21:36:10 by vantonie         ###   ########.fr       */
+/*   Created: 2023/02/18 17:40:27 by vantonie          #+#    #+#             */
+/*   Updated: 2023/02/22 19:05:40 by vantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <cstring>
+#include <string>
 
-char	*megaphone(char *string) {
-	toupper()
-}
+using std::cout;
+using std::endl;
+using std::string;
 
-int	main(int argc, char *argv) {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else 
-		megaphone(argv);
+string	uppercase(string str) {
+	string res;
 	
+	for(int i = 0; i < str.length(); i++)
+		res += toupper(str[i]);
+	return (res);
 }
 
+int	main(int argc, char **argv) {
+	string strCapitalized;
+
+	strCapitalized = uppercase(argv[1]);
+	cout << strCapitalized << endl;
+	return (0);
+}
