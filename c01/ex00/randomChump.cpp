e8b296663965811537f07ae70eda513c034116fd
Van Antonietti <v.antonietti@icloud.com>
2023-08-04 13:08:20 +0000

    Code Pattern Implementation

--- c01/ex00/randomChump.cpp
+++ c01/ex00/randomChump.cpp
@@ -6,13 +6,13 @@
 /*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 14:38:03 by vantonie          #+#    #+#             */
-/*   Updated: 2023/06/27 22:33:17 by vantonie         ###   ########.fr       */
+/*   Updated: 2023/08/04 12:38:45 by vantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void Zombie::randomChump( std::string name ) {
+void Zombie::randomChump(std::string name) {
   Zombie chump(name);
 
   chump.announce();
