b549a7a056b432ca3b24e876bdc1f8290db3c86e
Van Antonietti <v.antonietti@icloud.com>
2023-08-27 20:56:20 +0000

    test2

--- c01/ex00/newZombie.cpp
+++ c01/ex00/newZombie.cpp
@@ -6,13 +6,12 @@
 /*   By: vantonie <vantonie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 14:38:00 by vantonie          #+#    #+#             */
-/*   Updated: 2023/08/27 20:54:29 by vantonie         ###   ########.fr       */
+/*   Updated: 2023/08/27 20:56:09 by vantonie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* Zombie::newZombie( std::string name ) {
-  int i = 0;
   return (new Zombie(name));
 }
